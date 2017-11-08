21854b98b1ca69113dd2dc7bf4979145a32be641
VEGI <VEGI@VEGI-HP>
2017-11-08 21:56:50 +0530

    FirstCommit

---
+++ src/demopackage/democlass.java
@@ -0,0 +1,13 @@
+package demopackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class democlass 
+{
+@Test
+public void demoA()
+{
+	Reporter.log("Helloooo",true);
+}
+}
