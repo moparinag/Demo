a11a646499640f7f8036a82a543590a966d79f17
VEGI <VEGI@VEGI-HP>
2017-11-08 22:06:19 +0530

    Second Commit

---
+++ src/demopackage/democlassA.java
@@ -0,0 +1,13 @@
+package demopackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class democlassA 
+{
+@Test
+public void testA()
+{
+	Reporter.log("Hiiiii.... hellloooo",true);
+}
+}
